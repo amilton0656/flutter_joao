8c7c28feb94fd181b212248909061e6ddb704079
amilton0656 <amilton0656@gmail.com>
2023-09-03 21:15:58 -0300

    xxx

---
+++ greengrocerfull/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:greengrocerfull/src/pages_routes/app_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Greengrocer',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white.withAlpha(190),
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: PagesRoutes.splashRoute,
+      getPages: AppPages.pages,
+    );
+  }
+}
